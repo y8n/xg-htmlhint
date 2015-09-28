e74a73c8f33dc3671b745e61b17046f79285bb45
YangJiyuan <yangjiyuan@meituan.com>
2015-09-28 11:27:46 +0800

    finish doctype-first

--- src/rules/doctype-first.js
+++ src/rules/doctype-first.js
@@ -1,17 +1,18 @@
 /**
  * Copyright (c) 2015, Yanis Wang <yanis.wang@gmail.com>
+ * Copyright (c) 2015, Yang Jiyuan <yangjiyuan@meituan.com>
  * MIT Licensed
  */
 HTMLHint.addRule({
     id: 'doctype-first',
     description: 'Doctype must be declared first.',
-    init: function(parser, reporter){
+    init: function(parser,reporter){
         var self = this;
         var allEvent = function(event){
             if(event.type === 'start' || (event.type === 'text' && /^\s*$/.test(event.raw))){
                 return;
             }
-            if((event.type !== 'comment' && event.long === false) || /^DOCTYPE\s+/i.test(event.content) === false){
+            if(event.type !== 'comment' || event.long || !/^DOCTYPE\s+/i.test(event.content)){
                 reporter.error('Doctype must be declared first.', event.line, event.col, self, event.raw);
             }
             parser.removeListener('all', allEvent);
--- test/rules/doctype-first.spec.js
+++ test/rules/doctype-first.spec.js
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2015, Yanis Wang <yanis.wang@gmail.com>
+ * Copyright (c) 2015, Yang Jiyuan <yangjiyuan@meituan.com>
  * MIT Licensed
  */
 
@@ -15,17 +16,34 @@ ruleOptions[ruldId] = true;
 describe('Rules: '+ruldId, function(){
 
     it('Doctype not be first should result in an error', function(){
-        var code = '<html></html>';
+        var code = '     <html></html><!-- this is commet -->';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
         expect(messages[0].line).to.be(1);
-        expect(messages[0].col).to.be(1);
+        expect(messages[0].col).to.be(6);
+
+        code = '<!-- comment --><!doctype html><html></html>';
+        messages = HTMLHint.verify(code,ruleOptions);
+        expect(messages.length).to.be(1);
+
+        code = '<!doctypee html><html></html>';
+        messages = HTMLHint.verify(code,ruleOptions);
+        expect(messages.length).to.be(1);
+
+
     });
 
     it('Doctype be first should not result in an error', function(){
+
+        //HTML5 style
         var code = '<!DOCTYPE HTML><html></html>';
         var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(0);
+
+        // not HTML5 style
+        code = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd"><html></html>';
+        messages = HTMLHint.verify(code,ruleOptions);
         expect(messages.length).to.be(0);
     });
 
