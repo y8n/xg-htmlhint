7a433c0cb1824f2869d7be109268ab797905b6ad
yangjiyuan <yjy972080142@gmail.com>
2015-12-01 11:09:04 +0800

    change warning text

--- src/rules/style-disabled.js
+++ src/rules/style-disabled.js
@@ -4,12 +4,12 @@
  */
 HTMLHint.addRule({
     id: 'style-disabled',
-    description: '<style> tags cannot be used.',
+    description: '<style> tags is not allowed.',
     init: function(parser, reporter){
         var self = this;
         parser.addListener('tagstart', function(event){
             if(event.tagName.toLowerCase() === 'style'){
-                reporter.warn('The <style> tag cannot be used.', event.line, event.col, self, event.raw);
+                reporter.warn('The <style> tag is not allowed.', event.line, event.col, self, event.raw);
             }
         });
     }
